eed2df7502e78fc7034b5bd6c527178c85a3b2ed
Thibault Jan Beyer <hi@thibaultjanbeyer.com>
2017-09-08 22:53:35 +0200

    Update 2017-09-08T20:53:34.258Z

--- DragSelect.js
+++ DragSelect.js
@@ -123,7 +123,7 @@ DragSelect.prototype._setupOptions = function( options ) {
   // Area has to have a special position attribute for calculations
   if( this.area !== document ) {
     var computedArea = getComputedStyle( this.area );
-    var isPositioned = computedArea.position === 'absolute' || computedArea.position !== 'relative' || computedArea.position !== 'fixed';
+    var isPositioned = computedArea.position === 'absolute' || computedArea.position === 'relative' || computedArea.position === 'fixed';
     if( !isPositioned ) { this.area.style.position = 'relative'; }
   }
 
