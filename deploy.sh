8f746781d13d8932ab5c756239d1921f1bfe5ae8
Thibault Beyer <tbeyer@MB00904.local>
2018-07-20 15:46:15 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 git checkout gh-pages &&
-git checkout master dist/ &&
+git checkout master dist/ .gitignore &&
 git reset &&
 mv dist/* . &&
 rm -r dist &&
-git add .
-# git commit -m 'update' &&
-# git push &&
-# git checkout master
+git add . &&
+git commit -m 'deploy updates' &&
+git push &&
+git checkout master
