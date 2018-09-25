efa9113853f95973cfd64e52b7120e3d4896382f
Thibault Beyer <tbeyer@MB00904.local>
2018-09-25 15:04:01 +0200

    add automatic test to replace manual test3

---
+++ test/constrained.spec.js
@@ -0,0 +1,43 @@
+
+import puppeteer from "puppeteer";
+
+const baseUrl = `file://${process.cwd()}/test`;
+let page;
+let browser;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+});
+
+afterAll(() => {
+  browser.close();
+});
+
+describe('Callbacks', () => {
+
+  it('selection should be constrained to the area with only contains one element', async () => {
+    await page.goto(`${baseUrl}/constrained.html`);
+
+    const mouse = page.mouse
+    // move to the middle of the page
+    await mouse.move(150, 150)
+    await mouse.down()
+    // move 300px down and to the end of the page
+    // steps are how often the mouse moves
+    await mouse.move(800, 450, {steps: 100})
+    await mouse.up()
+
+    let executesFn = await page.evaluate(() => {
+      var id = document.querySelector('.item.three').id;
+      return {
+        id,
+        callbackIds: window.callbackIds
+      }
+    });
+
+    expect(executesFn.callbackIds.length).toBe(1);
+    expect(executesFn.callbackIds[0]).toBe(executesFn.id);
+  });
+
+});
