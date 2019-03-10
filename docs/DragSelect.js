2b4fe38bd2591b2f77708b4919e21d0f0cde63f7
Thibault Jan Beyer <hi@thibaultjanbeyer.com>
2019-03-10 18:48:56 +0100

    bump version

--- docs/DragSelect.js
+++ docs/DragSelect.js
@@ -10,7 +10,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-// v 1.10.0
+// v 1.11.0
 // @ts-check
 
 /* 
