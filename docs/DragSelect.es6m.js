42c5cfab35e921ad05905285cffc1fbeb80f8fae
Thibault Jan Beyer <thibault-jan.beyer@klarna.com>
2020-11-04 17:42:04 +0100

    update version comment

--- docs/DragSelect.es6m.js
+++ docs/DragSelect.es6m.js
@@ -22,7 +22,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-// v 1.12.2
+// v 1.14.0
 // @ts-check
 
 /* 
