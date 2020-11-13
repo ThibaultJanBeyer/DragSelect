0403b0662362810a014ae0a5df461934016aa04d
Thibault Jan Beyer <thibault-jan.beyer@klarna.com>
2020-11-13 09:26:21 +0100

    fixes selection of elements outside of area

---
+++ src/modules/_isCollision.js
@@ -0,0 +1,34 @@
+/**
+ * Axis-Aligned Bounding Box Collision Detection.
+ * Imagine following Example:
+ * 
+ *
+ *        b01
+ *     a01[1]a02
+ *        b02      b11
+ *              a11[2]a12
+ *                 b12
+ *
+ * 
+ * to check if those two boxes collide we do this AABB calculation:
+ * 1. a01 < a12 (left border pos box1 smaller than right border pos box2)
+ * 2. a02 > a11 (right border pos box1 larger than left border pos box2)
+ * 3. b01 < b12 (top border pos box1 smaller than bottom border pos box2)
+ * 4. b02 > b11 (bottom border pos box1 larger than top border pos box2)
+ * {@link https://en.wikipedia.org/wiki/Minimum_bounding_box#Axis-aligned_minimum_bounding_box Wikipedia}
+ * {@link https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection MDN}
+ * @param {{x:number,y:number,w:number,h:number}} a
+ * @param {{x:number,y:number,w:number,h:number}} b
+ */
+export default (el1, el2) => {
+  if (
+    el1.x < el2.x + el2.w && // 1.
+    el1.x + el1.w > el2.x && // 2.
+    el1.y < el2.y + el2.h && // 3.
+    el1.h + el1.y > el2.y    // 4.
+  ) {
+    return true // collision detected!
+  } else {
+    return false
+  }
+}
--- src/modules/_isElementTouching.js
+++ src/modules/_isElementTouching.js
@@ -1,5 +1,5 @@
 import '../types.js'
-import { _getScroll } from './'
+import { _getScroll, _getAreaRect, _isCollision } from './'
 
 /**
  * Checks if there is a collision between the element and the selector
@@ -20,6 +20,14 @@ export default (element, selector, zoom, area) => {
     w: selector.offsetWidth,
   }
 
+  const areaR = _getAreaRect(area)
+  const areaRect = {
+    y: areaR.top,
+    x: areaR.left,
+    h: areaR.height,
+    w: areaR.width
+  }
+
   const rect = element.getBoundingClientRect()
   const elementRect = {
     y: rect.top / zoom + scroll.y,
@@ -28,27 +36,10 @@ export default (element, selector, zoom, area) => {
     w: rect.width / zoom,
   }
 
-  // Axis-Aligned Bounding Box Collision Detection.
-  // Imagine following Example:
-  //    b01
-  // a01[1]a02
-  //    b02      b11
-  //          a11[2]a12
-  //             b12
-  // to check if those two boxes collide we do this AABB calculation:
-  //& a01 < a12 (left border pos box1 smaller than right border pos box2)
-  //& a02 > a11 (right border pos box1 larger than left border pos box2)
-  //& b01 < b12 (top border pos box1 smaller than bottom border pos box2)
-  //& b02 > b11 (bottom border pos box1 larger than top border pos box2)
-  // See: https://en.wikipedia.org/wiki/Minimum_bounding_box#Axis-aligned_minimum_bounding_box and https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
   if (
-    selectionRect.x < elementRect.x + elementRect.w &&
-    selectionRect.x + selectionRect.w > elementRect.x &&
-    selectionRect.y < elementRect.y + elementRect.h &&
-    selectionRect.h + selectionRect.y > elementRect.y
-  ) {
-    return true // collision detected!
-  } else {
-    return false
-  }
+    _isCollision(areaRect, elementRect) && 
+    _isCollision(selectionRect, elementRect)
+  ) return true
+
+  return false
 }
