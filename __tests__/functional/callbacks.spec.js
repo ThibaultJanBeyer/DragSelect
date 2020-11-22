8796e887eb6e5e153201be20ea6205e20034c22f
Thibault Jan Beyer <thibault-jan.beyer@klarna.com>
2020-11-22 18:06:39 +0100

    update tests

--- __tests__/functional/callbacks.spec.js
+++ __tests__/functional/callbacks.spec.js
@@ -1,17 +1,17 @@
-const baseUrl = `file://${process.cwd()}/__tests__/functional`;
+const baseUrl = `file://${process.cwd()}/__tests__/functional`
 
 describe('Callbacks', () => {
   it('should trigger callbacks with the correct elements when elements are selected', async () => {
-    await page.goto(`${baseUrl}/callbacks.html`);
+    await page.goto(`${baseUrl}/callbacks.html`)
 
-    const mouse = page.mouse;
+    const mouse = page.mouse
     // move to the middle of the page
-    await mouse.move(0, 0);
-    await mouse.down();
+    await mouse.move(1, 1)
+    await mouse.down()
     // move 300px down and to the end of the page
     // steps are how often the mouse moves
-    await mouse.move(200, 200, { steps: 100 });
-    await mouse.up();
+    await mouse.move(200, 200, { steps: 100 })
+    await mouse.up()
 
     let executesFn = await page.evaluate(() => {
       return {
@@ -19,15 +19,15 @@ describe('Callbacks', () => {
         onDragMoveCalls: window.onDragMoveCalls,
         onElementSelectCalls: window.onElementSelectCalls,
         onElementUnselectCalls: window.onElementUnselectCalls,
-        callbackCalls: window.callbackCalls
-      };
-    });
+        callbackCalls: window.callbackCalls,
+      }
+    })
 
-    expect(executesFn.onDragStartCalls.length).toBe(1);
-    expect(executesFn.onDragMoveCalls.length).toBe(100);
-    expect(executesFn.onElementSelectCalls.length).toBe(2);
-    expect(executesFn.onElementUnselectCalls.length).toBe(0);
-    expect(executesFn.callbackCalls.length).toBe(1);
-    expect(executesFn.callbackCalls[0].elements.length).toBe(2);
-  });
-});
+    expect(executesFn.onDragStartCalls.length).toBe(1)
+    expect(executesFn.onDragMoveCalls.length).toBe(100)
+    expect(executesFn.onElementSelectCalls.length).toBe(2)
+    expect(executesFn.onElementUnselectCalls.length).toBe(0)
+    expect(executesFn.callbackCalls.length).toBe(1)
+    expect(executesFn.callbackCalls[0].elements.length).toBe(2)
+  })
+})
