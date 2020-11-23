f014e2c029e153f4862fa754af0fbc54b49b8d01
Thibault Jan Beyer <thibault-jan.beyer@klarna.com>
2020-11-23 21:54:31 +0100

    add version banner

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,40 @@
 import babel from '@rollup/plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
-import { terser } from "rollup-plugin-terser"
+import { terser } from 'rollup-plugin-terser'
 import fs from 'fs'
 import path from 'path'
+
+const banner = `/*
+
+ ~~~ Version ${process.env.npm_package_version} ~~~
+
+ ******************************************
+
+    ____                   _____      __          __ 
+   / __ \\_________ _____ _/ ___/___  / /__  _____/ /_
+  / / / / ___/ __ \`/ __ \`/\\__ \\/ _ \\/ / _ \\/ ___/ __/
+ / /_/ / /  / /_/ / /_/ /___/ /  __/ /  __/ /__/ /_  
+/_____/_/   \\__,_/\\__, //____/\\___/_/\\___/\\___/\\__/  
+               /____/                              
+
+ ******************************************
+ 
+ {*} {*} STAR THIS PLUGIN ON GITHUB {*} {*}
+
+ https://github.com/ThibaultJanBeyer/DragSelect
+ Please give it a like, this is what makes me happy :-)
+ Thank You
+
+ {*} {*} STAR THIS PLUGIN ON GITHUB {*} {*}
+
+ ******************************************
+ ********* The MIT License (MIT) **********
+ ******************************************
+ Created 2017 by ThibaultJanBeyer
+ web: http://www.thibaultjanbeyer.com/
+ github: https://github.com/ThibaultJanBeyer/DragSelect
+
+*/`
 
 export default {
   input: 'src/DragSelect.js',
@@ -11,26 +43,28 @@ export default {
       file: 'dist/DragSelect.es6m.js',
       format: 'es',
       name: 'DragSelect',
+      banner: banner,
     },
     {
       file: 'dist/DragSelect.js',
       format: 'umd',
       name: 'DragSelect',
+      banner: banner,
     },
     {
       file: 'dist/ds.es6m.min.js',
       format: 'es',
       name: 'DragSelect',
       compact: true,
-      plugins: [terser()]
+      plugins: [terser()],
     },
     {
       file: 'dist/ds.min.js',
       format: 'umd',
       name: 'DragSelect',
       compact: true,
-      plugins: [terser()]
-    }
+      plugins: [terser()],
+    },
   ],
   plugins: [
     resolve(),
@@ -38,17 +72,17 @@ export default {
     {
       name: 'copy',
       writeBundle(options) {
-        fs.copyFileSync(options.file, `docs/${path.basename(options.file)}`);
+        fs.copyFileSync(options.file, `docs/${path.basename(options.file)}`)
         console.log(options.file, `docs/${path.basename(options.file)}`)
-      }
+      },
     },
     {
-      name: "disable-treeshaking",
+      name: 'disable-treeshaking',
       transform(code, id) {
         if (id.endsWith('types.js')) {
-          return {moduleSideEffects: 'no-treeshake'};
-        } 
-      }
-    }
+          return { moduleSideEffects: 'no-treeshake' }
+        }
+      },
+    },
   ],
-};
+}
