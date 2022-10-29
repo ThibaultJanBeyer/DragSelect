9cb12433f508ac04b60c2c42653ab6a9337cdd39
Thibault Jan Beyer <thibault-jan.beyer@klarna.com>
2022-10-29 16:55:33 +0200

    add uniqueness to toArray method

--- src/methods/toArray.js
+++ src/methods/toArray.js
@@ -2,7 +2,7 @@
 import '../types'
 
 /**
- * Transforms any list or single item to an array so user doesn’t have to care.
+ * Transforms any list or single dom node to an array so user doesn’t have to care. Also removes duplicates.
  * @param {DSInputElements} items a single item, a Node-list or any element group
  * @return {DSElements}
  */
@@ -13,5 +13,6 @@ export default (items) => {
     (items instanceof HTMLElement || items instanceof SVGElement)
   )
     return [items]
-  return [...items]
+  // @ts-ignore
+  return [...new Set([...items])]
 }
