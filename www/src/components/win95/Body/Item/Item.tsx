8fa2045c8388d45f6bec662c86dd72e5a8c917f9
Thibault Jan Beyer <thibault.beyer@gmail.com>
2023-09-25 07:07:20 +0200

    remove deprecations

--- www/src/components/win95/Body/Item/Item.tsx
+++ www/src/components/win95/Body/Item/Item.tsx
@@ -1,64 +1,64 @@
-import React, { memo, useEffect, useRef, useState } from 'react'
-import { BubbleExplosion } from 'beautiful-web-animations'
+import React, { memo, useEffect, useRef, useState } from "react";
+import { BubbleExplosion } from "beautiful-web-animations";
 
-import styles from './Item.module.scss'
-import { FileIcon } from './FileIcon'
-import { getRandomFileName } from './fileNames'
-import { DSPubCallback, DSInputElement, useDragSelect } from '../../../DragSelectContext'
+import styles from "./Item.module.scss";
+import { FileIcon } from "./FileIcon";
+import { getRandomFileName } from "./fileNames";
+import { DSPubCallback, useDragSelect } from "../../../DragSelectContext";
 
 const explode = async (element: HTMLElement, isAppearing?: boolean) => {
   const BE = BubbleExplosion({
     element,
     content: `url("${element
-      .querySelector('img[data-img]')
-      ?.getAttribute('data-img')}")`,
-    particles: { direction: isAppearing ? 'up' : 'down', size: 15 },
+      .querySelector("img[data-img]")
+      ?.getAttribute("data-img")}")`,
+    particles: { direction: isAppearing ? "up" : "down", size: 15 },
     isAppearing,
-  })
-  await BE.trigger()
-  BE.destroy()
-}
+  });
+  await BE.trigger();
+  BE.destroy();
+};
 
 const _Item: React.FC = () => {
-  const [available, setAvailable] = useState(true)
-  const ds = useDragSelect()
-  const inputEl = useRef(null)
+  const [available, setAvailable] = useState(true);
+  const ds = useDragSelect();
+  const inputEl = useRef(null);
 
   useEffect(() => {
-    const element = inputEl.current as unknown as HTMLElement
-    if (!element || !ds || !available) return
+    const element = inputEl.current as unknown as HTMLElement;
+    if (!element || !ds || !available) return;
 
-    const cb: DSPubCallback<"DS:removed", DSInputElement> = async ({ item }) => {
-      if (item !== element) return
-      ds.unsubscribe('DS:removed', cb)
-      await explode(element)
-      setAvailable(false)
-    }
+    const cb: DSPubCallback<"DS:removed"> = async ({ item }) => {
+      if (item !== element) return;
+      ds.unsubscribe("DS:removed", cb);
+      await explode(element);
+      setAvailable(false);
+    };
 
     const firstEffect = async () => {
-      await explode(element, true)
-      ds.addSelectables(element)
-      ds?.subscribe('DS:removed', cb)
-    }
+      await explode(element, true);
+      ds.addSelectables(element);
+      ds?.subscribe("DS:removed", cb);
+    };
 
-    if (!ds.SelectableSet.has(element)) firstEffect()
+    if (!ds.SelectableSet.has(element)) firstEffect();
 
     return () => {
-      if (!element || !ds) return
-      ds.unsubscribe('DS:removed', cb)
-    }
-  }, [ds, inputEl, available])
+      if (!element || !ds) return;
+      ds.unsubscribe("DS:removed", cb);
+    };
+  }, [ds, inputEl, available]);
 
-  if (!available) return null
+  if (!available) return null;
 
-  const name = getRandomFileName()
+  const name = getRandomFileName();
 
   return (
     <button className={styles.root} ref={inputEl} aria-labelledby={name}>
       <FileIcon />
       <div>{name}</div>
     </button>
-  )
-}
+  );
+};
 
-export const Item = memo(_Item)
+export const Item = memo(_Item);
